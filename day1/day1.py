84b06adc549a1b3f4fc79b945034fcb2712bad39
Mbooo <mboreha58@hotmail.fr>
2021-12-02 19:49:10 +0100

    Day 1

--- day1/day1.py
+++ day1/day1.py
@@ -14,20 +14,23 @@ def part1():
     print("Nombre de mesure supérieure à la précédente : ",increases)
 
 def part2():
-    sums = []
+    #sums = []
     sumIncreases = 0
-    for i in range(len(depths) - 2):
-        sums.append(depths[i] + depths[i+1] + depths[i+2])
-    for j in range(len(sums) - 1):
-        if sums[j] < sums[j+1]:
+    # for i in range(len(depths) - 2):
+    #     sums.append(depths[i] + depths[i+1] + depths[i+2])
+    # for j in range(len(sums) - 1):
+    #     if sums[j] < sums[j+1]:
+    #         sumIncreases += 1
+    for i in range(len(depths) - 3):
+        if depths[i] < depths[i+3]:
             sumIncreases += 1
 
-    print("Nombre de sommes supérieure à la précédente : ", sumIncreases)
-
+    print("Nombre de sommes de triplet supérieure à la précédente : ", sumIncreases)
 
 
 part1()
 part2()
 end = time.time()
 elapsed = end - start
+
 print(f"Temps d'éxecution : {elapsed : .2}ms")
